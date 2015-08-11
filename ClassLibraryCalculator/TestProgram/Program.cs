68ddef214231b737f24a0e4d9369e57c2bfac732
Nikita Artes <nikas.artes@byndyusoft.com>
2015-08-11 11:17:17 +0500

    Добавил логирование log4net

--- ClassLibraryCalculator/TestProgram/Program.cs
+++ ClassLibraryCalculator/TestProgram/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using ClassLibraryCalculator;
+using log4net;
 
 namespace TestProgram
 {
@@ -9,10 +10,16 @@ namespace TestProgram
         {
             try
             {
+                var logger = LogManager.GetLogger(typeof (Program));
+                log4net.Config.DOMConfigurator.Configure();
                 var calculator = new CalculationWithRPN();
                 calculator.AddOperator(new PlusOperator()).AddOperator(new MinusOperator()).AddOperator(new DivisionOperator()).AddOperator(new MultiplicationOperator());
                 Console.WriteLine("Add the mathematical expression");
-                Console.WriteLine(calculator.Calculation(Console.ReadLine()).Name);
+                
+                var result = calculator.Calculation(Console.ReadLine());
+                logger.Info(string.Format("Результат = {0}", result.Name));
+
+                Console.WriteLine(result.Name);
                 Console.ReadKey();
             }
             catch (Exception e)
